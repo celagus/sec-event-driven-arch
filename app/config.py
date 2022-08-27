2cef0d8ac18150bbb5a6f1a9161cc45e9d73e4d7
Agustin Celano <agustin.celano@pomelo.la>
2022-08-27 11:06:25 -0300

    update sqs_url to get dynamic

--- app/config.py
+++ app/config.py
@@ -1,8 +1,12 @@
+import boto3
+
 AWS_CONFIG = {
-    "sqs_url": "<your-sqs-arn-here>",
     "sqs_name": "sec-event-driven-arch.fifo",
     "aws_region": "us-east-1",
     "table_name": "sec-event-driven-arch",
     "ip_set_name": "malicious_actors",
     "ban_lapse": dict(critical=43200, high=10080, medium=1440, low=60)
 }
+
+sts = boto3.client("sts")
+AWS_CONFIG["sqs_url"] = f"https://sqs.us-east-1.amazonaws.com/{sts.get_caller_identity()['Account']}/{AWS_CONFIG['sqs_name']}"
